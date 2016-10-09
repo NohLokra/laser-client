c0f11d1e68bdf4ab8157df362951e79fe02508ba
Zozoleclown <Zozoleclown1@gmail.com>
2016-10-09 22:41:00 +0200

    Ajout du scoreParser et des classes qu'il sous entends

--- laser_save.pro
+++ laser_save.pro
@@ -1,10 +1,12 @@
 SOURCES += \
     main.cpp \
     mainwindow.cpp \
-    apiservice.cpp
+    apiservice.cpp \
+    scoreparser.cpp
 
 QT += widgets network
 
 HEADERS += \
     mainwindow.h \
-    apiservice.h
+    apiservice.h \
+    scoreparser.h
--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include "mainwindow.h"
 #include "apiservice.h"
 
+#include "scoreparser.h"
+
 int main( int argc, char** argv )
 {
     QApplication app(argc, argv);
     MainWindow mw;
+
+    ScoreParser sp("test");
 
     mw.show();
     return app.exec();
